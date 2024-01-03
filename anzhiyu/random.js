17a74ed32e061b3b6e6be58726880174bf444ef4
NimikyZhong <nimikyzhong@gmail.com>
2024-01-03 21:42:49 +0800

    Site updated: 2024-01-03 21:42:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/01/My-New-Post/","2024/01/01/hello-world/"];function toRandomPost(){
+var posts=["2024/01/03/2023年总结/","2024/01/01/hello-world/","2024/01/01/My-New-Post/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
